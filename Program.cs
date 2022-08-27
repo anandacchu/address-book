363d4d2d277a0b3494d2be70ecc5b6a12faa438f
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-08-27 08:29:39 +0530

    master

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassAndObject
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            int a = 10;
+            Vechile obj = new Vechile();
+            obj.run();
+
+            
+            Console.WriteLine(obj.colour);
+            Console.WriteLine(obj.wheels);
+
+        }
+    }
+}
