accbfb1063a7bb46d138ece7362e9c095c079679
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-08-27 08:28:39 +0530

    uc3

---
+++ Vechile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    public class Vechile
+    {
+        public string colour;
+        public int wheels;
+        {
+        public class
+         colour = white;
+         wheels = 4;
+        }
+       public vechile(string colourofvechile,int wheel )
+
+        public void run()
+        {
+            Console.WriteLine("Vechile will move from one place to another");
+        }
+    }
+}
